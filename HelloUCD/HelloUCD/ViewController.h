2d1bff8ec7bfe57060e752b146e89489edcd5a0a
Sinapse87 <lucali87@gmail.com>
2014-01-08 21:03:52 +0000

    HELLO UCD added

---
+++ HelloUCD/HelloUCD/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloUCD
+//
+//  Created by LUCA ALIBERTI on 08/01/2014.
+//  Copyright (c) 2014 UCD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
