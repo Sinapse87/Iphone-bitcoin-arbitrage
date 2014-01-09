c53a992827a1aeabd0539fc80882d0e9ec62ed32
Sinapse87 <lucali87@gmail.com>
2014-01-09 22:45:42 +0000

    HelloPoly very first version. Needs improvements.

---
+++ HelloPoly/HelloPoly/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloPoly
+//
+//  Created by LUCA ALIBERTI on 09/01/2014.
+//  Copyright (c) 2014 comp41550. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HelloPolyAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HelloPolyAppDelegate class]));
+    }
+}
---
+++ HelloPoly/HelloPolyViewController.h
@@ -0,0 +1,13 @@
+//
+//  HelloPolyViewController.h
+//  HelloPoly
+//
+//  Created by comp41550 on 09/01/2014.
+//  Copyright (c) 2014 comp41550. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloPolyViewController : UIViewController
+
+@end
