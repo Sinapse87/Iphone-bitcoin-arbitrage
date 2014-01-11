def1ae1423f7d6bbe5820d2ed76773183842ac68
Sinapse87 <lucali87@gmail.com>
2014-01-11 16:25:06 +0000

    Chaged directory structure

---
+++ Assignment 2/HelloPoly/HelloPolyAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HelloPolyAppDelegate.h
+//  HelloPoly
+//
+//  Created by LUCA ALIBERTI on 09/01/2014.
+//  Copyright (c) 2014 comp41550. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloPolyAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
